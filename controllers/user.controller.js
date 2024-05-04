1d6b5cafb46d0f4cc85e3b95609f23c5839ca874
chocoscoding <chocoscoding@gmail.com>
2024-05-04 09:20:00 -0500

    post count for user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,15 +4,19 @@ import { v2 as cloudinary } from "cloudinary";
 // models
 import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
+import Post from "../models/post.model.js";
 
 export const getUserProfile = async (req, res) => {
 	const { username } = req.params;
 
 	try {
 		const user = await User.findOne({ username }).select("-password");
-		if (!user) return res.status(404).json({ message: "User not found" });
 
-		res.status(200).json(user);
+		if (!user) return res.status(404).json({ message: "User not found" });
+		//get post count
+		const postCount = await Post.countDocuments({ user: user._id });
+
+		res.status(200).json({ ...user.toObject(), postCount });
 	} catch (error) {
 		console.log("Error in getUserProfile: ", error.message);
 		res.status(500).json({ error: error.message });
